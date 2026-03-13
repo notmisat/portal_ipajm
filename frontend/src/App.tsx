8fc7197a73fa11b9ed99a8416ca2382e914001e5
Davi Fernandes de Jesus Roque <davi.roque@previdencia.local>
2026-03-13 15:33:31 -0300

    Estruturação do Backend (IGNORAR COMMIT ANTERIOR)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { Suspense } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
-import { router } from './app/routes';
+import { router } from './routes/routes';
 import { Toaster } from './components/ui/sonner';
 
 export default function App() {
